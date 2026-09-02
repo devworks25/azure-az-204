875e33f82e3fe5a77b8f304a5f9b820bda06d2a2
Deepak Kumar <dkmagento@gmail.com>
2026-09-03 02:53:54 +0530

    fix 6

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var message =  process.env.MESSAGE;
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', message: message });
+  console.log('========== ENV TEST ==========');
+  console.log('MESSAGE:', process.env.MESSAGE);
+  console.log('PORT:', process.env.PORT);
+  console.log('==============================');
+
+  res.send(`
+    <h1>Environment Test</h1>
+    <p>MESSAGE: ${process.env.MESSAGE || 'UNDEFINED'}</p>
+    <p>PORT: ${process.env.PORT || 'UNDEFINED'}</p>
+  `);
 });
 
 module.exports = router;
